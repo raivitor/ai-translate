af7ae74e3151d22b9a3604cac7416a386e1e7a34
Raí Vitor <raivitor@gmail.com>
2026-05-16 20:39:44 -0300

    chore: remove deprecated bootstrap.js file after migration to Electron/Vite

--- scripts/bootstrap.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-
-console.warn(
-  'bootstrap.js pertence ao boilerplate original e foi desativado apos a migracao para Electron/Vite.',
-)
-console.warn('Ajuste package.json e README diretamente para novos nomes de projeto.')
